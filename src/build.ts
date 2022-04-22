95847d772730b0a872e08200264da641190e3471
wenxue.cwx <coolman_84@163.com>
2022-04-22 09:36:26 +0800

    export md2html

--- src/build.ts
+++ src/build.ts
@@ -1,7 +1,11 @@
 import _MarkdownEditor from './components/editor/markdown/index.vue'
+import _md2html from './components/editor/markdown/parser/md2html'
 import * as MarkdownEditorTypes from './components/editor/markdown/types'
 
 export const MarkdownEditor = _MarkdownEditor
+export const utils = {
+  md2html: _md2html
+}
 
 export type MarkdownEditorProps = MarkdownEditorTypes.TotonooMarkdownEditorProps
 export type ThemeType = MarkdownEditorTypes.ThemeType
